1ad1b911750f222bdd4cbbd3311f0ffdcc5faec5
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2025-03-14 01:17:10 +0100

    added workshops

--- utils.lua
+++ utils.lua
@@ -1,12 +1,43 @@
-function escape_latex(str)
+function escapeLatex(str)
     return str:gsub("\\", "\\textbackslash ")
         :gsub("([%%%$#_{}~^&])", "\\%1")
         :gsub("_", "\\_")
 end
 
-function load_datasets(filename)
+function loadJson(filename)
     local file = io.open(filename, "r")
     local content = file:read("*all")
     file:close()
-    return utilities.json.tolua(content)
+    local lua = utilities.json.tolua(content)
+    if not lua then
+        tex.print([[\textbf{Fehler: Konnte ]] .. filename .. [[ nicht laden!}]])
+        error()
+    end
+    return lua
+end
+
+function centeredImage(filename)
+    local wrapperBegin = [[\begin{center}]]
+    
+    local image = (filename and filename ~= "")  
+            and [[\includegraphics[width=\textwidth,height=8cm,keepaspectratio]{]] .. filename .. [[}]]
+            or [[\framebox[\textwidth][c]{\parbox{8cm}{\centering Bildplatzhalter}}]]
+            
+    local wrapperEnd = [[\end{center}  \\ ]]
+
+    return (wrapperBegin .. image .. wrapperEnd) 
+end
+
+function outputLatex(str)
+    local output_cleaned, _ = str :gsub("[\n\r]", " ")
+    tex.print(output_cleaned)
+end
+
+function downloadWithQRCode(url)
+    return [[
+        \url{]] .. url .. [[} \\
+        \begin{center}
+            \qrcode[height=3cm]{]] .. escapeLatex(url) .. [[}
+        \end{center}
+    ]]
 end
